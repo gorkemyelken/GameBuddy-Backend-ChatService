6c2625374b753c80300dadc3dd36196421c998c8
Görkem <gorkem_yelken@hotmail.com>
2024-11-01 13:09:29 +0300

    WebSocketConfig updated.

--- src/main/java/com/gamebuddy/chat/configuration/WebSocketConfig.java
+++ src/main/java/com/gamebuddy/chat/configuration/WebSocketConfig.java
@@ -11,7 +11,9 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS(); // Gerekli ise SockJS desteği eklenebilir
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("http://localhost:3000", "https://yourdomain.com") // İzin verilen origin'leri belirtin
+                .withSockJS();
     }
 
     @Override
